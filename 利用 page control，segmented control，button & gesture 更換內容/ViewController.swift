3f163599b9949991935509e440c8c06dee882343
Ron <onedarksky@icloud.com>
2021-11-04 12:01:26 +0800

    Initial Commit

---
+++ 利用 page control，segmented control，button & gesture 更換內容/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  利用 page control，segmented control，button & gesture 更換內容
+//
+//  Created by Jiang Yung Tse on 2021/11/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
